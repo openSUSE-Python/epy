04ee6477de3294447c823decbad1a42ea5f2dc5c
Tomasz Kapias <tomasz@tkapias.net>
2023-12-26 14:19:15 +0700

    add support for speechd | piper

--- src/epy_reader/speakers/__init__.py
+++ src/epy_reader/speakers/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["SpeakerBaseModel", "SpeakerMimic", "SpeakerPico", "SpeakerGttsMPV"]
+__all__ = ["SpeakerBaseModel", "SpeakerSpeechd", "SpeakerMimic", "SpeakerPico", "SpeakerGttsMPV"]
 
 from epy_reader.speakers.base import SpeakerBaseModel
 from epy_reader.speakers.speechd import SpeakerSpeechd
